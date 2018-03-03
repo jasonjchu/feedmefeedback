83d08df249573a127c9921fc4840dc17327e7f24
Jason Chu <jason368@hotmail.ca>
2018-03-03 18:55:23 -0500

    App is in minimal state

--- app/src/main/java/starterhacks/jwj/starterhacks/MainActivity.java
+++ app/src/main/java/starterhacks/jwj/starterhacks/MainActivity.java
@@ -15,14 +15,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_to_do:
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_scan:
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_rewards:
                     return true;
             }
             return false;
@@ -34,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
