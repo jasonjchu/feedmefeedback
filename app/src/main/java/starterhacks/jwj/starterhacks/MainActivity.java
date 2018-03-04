4f50b2fdcfd25c5dd464be5db789cf8e315c9e43
Jason Chu <jason368@hotmail.ca>
2018-03-04 01:38:31 -0500

    Can add items to to do list

--- app/src/main/java/starterhacks/jwj/starterhacks/MainActivity.java
+++ app/src/main/java/starterhacks/jwj/starterhacks/MainActivity.java
@@ -6,13 +6,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
 import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> incomplete = new ArrayList<String>();
 
     public String code = "INIT";
 
@@ -27,11 +32,14 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_to_do:
+                    updateListToDo();
                     return true;
                 case R.id.navigation_scan:
                     getScanner();
                     return true;
                 case R.id.navigation_rewards:
+                    TextView view2 = (TextView) findViewById(R.id.message);
+                    view2.setText("Rewards");
                     return true;
             }
             return false;
@@ -50,11 +58,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        TextView view = (TextView) findViewById(R.id.message);
         if (resultCode == RESULT_OK) {
             code = data.getStringExtra("SCAN_RESULT");
-            TextView view = (TextView) findViewById(R.id.message);
-            view.setText("Success!\nCode: " + code);
+            view.setText("Success!");
             System.out.println(code);
+        } else {
+            view.setText("Failure!");
         }
+        String append = code;
+        incomplete.add(append);
+        ListView lview1 = (ListView) findViewById(R.id.lView1);
+        lview1.setVisibility(lview1.GONE);
+    }
+
+    private void updateListToDo(){
+        TextView view1 = (TextView) findViewById(R.id.message);
+        view1.setText("To Do");
+        ListView lv1 = (ListView) findViewById(R.id.lView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,incomplete.toArray(new String[incomplete.size()]));
+        lv1.setAdapter(adapter);
+        lv1.setVisibility(View.VISIBLE);
     }
 }
