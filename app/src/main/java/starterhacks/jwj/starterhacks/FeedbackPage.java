4b23b0e7d3b1304ba0b5dcf5151bb5159ef9fe6a
Jason Chu <jason368@hotmail.ca>
2018-03-04 12:58:36 -0500

    Can connect to the internet and retreive JSON data

--- app/src/main/java/starterhacks/jwj/starterhacks/FeedbackPage.java
+++ app/src/main/java/starterhacks/jwj/starterhacks/FeedbackPage.java
@@ -10,37 +10,49 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 public class FeedbackPage extends AppCompatActivity {
+
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        queue = Volley.newRequestQueue(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback_page);
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("email", "will@mail.com" );
+
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, "http://httpbin.com/post", null, new Response.Listener<JSONObject>() {
+                (Request.Method.POST, "http://feed-me-back.herokuapp.com/users/get-questions", new JSONObject(params), new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        TextView view = null;
-                        view.setText(response.toString());
-                        LinearLayout layout = findViewById(R.id.feedPage);
-                        layout.addView(view);
                         System.out.println("Response: " + response.toString());
+                        final TextView view = new TextView(getApplicationContext());
+                        final LinearLayout layout = findViewById(R.id.feedPage);
+                        view.setText(response.toString());
+                        layout.addView(view);
                     }
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
+                        System.out.println("Failure! " + error.toString());
 
                     }
                 });
+        queue.add(jsObjRequest);
     }
 }
